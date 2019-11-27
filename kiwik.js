5c18fafbb9f78abfe71fedfffdfab62d4b46dc0a
Gatti <florenciaGatti@users.noreply.github.com>
2019-11-27 19:53:58 -0300

    Primer comit

---
+++ kiwik.js
@@ -0,0 +1,18 @@
+cont app = New Vue ({
+    el: '#app',
+    data:{
+        //categorias
+        personajes:[
+            //nombre de los arcchivos
+            {nombre:'imagenes/personaje1.jpg'},
+            {nombre:'imagenes/personaje2.jpg'},
+            {nombre:'imagenes/personaje3.jpg'},
+            {nombre:'imagenes/personaje4.jpg'},
+        ],
+        animales:[
+            {nombre:'imagenes/animales1.jpg'},
+
+        ]
+
+    }
+})
